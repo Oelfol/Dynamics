c50987ee03586c3509ee12abebfb04147a918255
Beth <69660862+Oelfol@users.noreply.github.com>
2021-01-12 09:58:20 -0600

    Update IBMQSetup.py

--- HeisenbergCodes/IBMQSetup.py
+++ HeisenbergCodes/IBMQSetup.py
@@ -27,8 +27,7 @@ import numpy as np
 # Sometimes, this command is not necessary (if console says no account in session).
 # IBMQ.disable_account()
 
-TOKEN = '29e5a75de595227f8e2477aa7abab6595dfafbc3375d7b510fa8be7c03b95c031e49887' \
-        'af3da2d18efb30719038730cdf0ee806523bfdfbc4c231f0ad93f7e74'
+#TOKEN = 
 provider = IBMQ.enable_account(TOKEN, hub='ibm-q', group='open', project='main')
 simulator = Aer.get_backend('qasm_simulator')
 
